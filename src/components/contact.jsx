6b3bf57aaa58fdc2466d218fbb0942b0ec862fdc
Bikram Sharma <sharmabikram061@gmail.com>
2025-07-27 13:33:45 +0600

    made responsive

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,98 +1,137 @@
-import React from 'react'
-
+import React from "react";
 
 export default function Contact() {
+  const [result, setResult] = React.useState("");
 
-    const [result, setResult] = React.useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-    const onSubmit = async (event) => {
-      event.preventDefault();
-      setResult("Sending....");
-      const formData = new FormData(event.target);
+    const access_key = import.meta.env.VITE_ACCESS_KEY;
 
-      const access_key = import.meta.env.VITE_ACCESS_KEY;
-  
-      formData.append("access_key", access_key);
-  
-      const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        body: formData
-      });
-  
-      const data = await response.json();
-  
-      if (data.success) {
-        setResult("Message Submitted Successfully");
-        event.target.reset();
-      } else {
-        console.log("Error", data);
-        setResult(data.message);
-      }
-    };
+    formData.append("access_key", access_key);
 
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Message Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
 
   return (
-    <div id='contact' className='text-white sm:h-[100vh] p-10 sm:p-20 sm:grid grid-cols-2 grid-rows-6 place-items-center gap-4'>
+    <div
+      id="contact"
+      className="text-white sm:h-[100vh] p-10 sm:p-20 sm:grid grid-cols-2 grid-rows-6 place-items-center gap-4"
+    >
+      <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent col-span-2 sm:text-5xl text-3xl">
+        Contacts
+      </div>
 
-        <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent col-span-2 text-5xl'>
-            Contacts
-        </div>
-
-        <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-4xl'>
+      <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent sm:text-4xl text-2xl ">
         Let's talk
+      </div>
+
+      <div className="row-start-3">
+        I'm currently avaliable to take on new projects, so feel free to send me
+        a message about anything that you want me to work on. You can contact
+        anytime.
+      </div>
+
+      <div className="row-start-4 w-full h-full grid grid-rows-2">
+        <p className="inline text-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="inline size-6 mr-2 "
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
+          </svg>
+          sharmabikram061@gmail.com
+        </p>
+
+        <p className="mt-5 inline">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="inline size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+            />
+          </svg>
+          +975-77232320 / +975-17825220
+        </p>
+      </div>
+
+      <form
+        onSubmit={onSubmit}
+        className="w-full h-full sm:grid col-start-2 row-start-2 row-span-4 grid-rows-5 gap-1 mt-5"
+      >
+        <div className="w-full h-full text-white">
+          <label htmlFor="name">Your Name</label>
+          <input
+            type="text"
+            name="name"
+            className="w-full h-10 border mt-2 rounded-lg bg-gray-900 px-2"
+            required
+          />
         </div>
 
-        <div className='row-start-3'>
-        I'm currently avaliable to take on new projects,
-        so feel free to send me a message about anything that you want me to work on. You can contact anytime.
-        </div>
-        
-        <div className='row-start-4 w-full h-full grid grid-rows-2'>
-            <p className='inline text-6'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline size-6 mr-2 ">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                </svg>
-                    sharmabikram061@gmail.com
-            </p>
-            
-            <p className='mt-5 inline'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline size-6">
-                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                </svg>
-                +975-77232320 / +975-17825220           
-                
-            </p>
-
+        <div className="w-full h-full">
+          <label htmlFor="email">Your Email</label>
+          <input
+            type="email"
+            name="email"
+            className="w-full h-10 border mt-2 rounded-lg bg-gray-900 px-2"
+            required
+          />
         </div>
 
-
-        <form onSubmit={onSubmit} className='w-full h-full sm:grid col-start-2 row-start-2 row-span-4 hidden  grid-rows-5 gap-1'>
-            <div className='w-full h-full text-white'>
-                <label htmlFor="name">Your Name</label>
-                <input type="text" name='name'  className='w-full h-10 border mt-2 rounded-lg bg-gray-900 px-2' required/>
-            </div>
-
-            <div className='w-full h-full'>
-            <label htmlFor="email">Your Email</label>
-            <input type="email" name='email' className='w-full h-10 border mt-2 rounded-lg bg-gray-900 px-2' required/>
-            </div>
-
-            <div className='row-span-2 w-full h-full'>
-            <label htmlFor="email">Write your message here</label>
-            <textarea name='message'   className='w-full h-10 border mt-2 h-30 rounded-lg bg-gray-900 px-2' required/>
-            </div>
-            <div className='w-full h-full flex items-center'>
-                <button type='submit' className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full h-4/6 px-5 '>{result? result : 'Submit now'}</button>
-            </div>
-
-        </form>
-
-
-        <div className='row-start-6 col-span-2  w-full h-full'>
-        <hr className='w-full bg-white' /><br />
-        <p className=''>&copy; 2025 Bikram Sharma</p>
-
+        <div className="row-span-2 w-full h-full">
+          <label htmlFor="email">Write your message here</label>
+          <textarea
+            name="message"
+            className="w-full border mt-2 h-30 rounded-lg bg-gray-900 px-2"
+            required
+          />
         </div>
-            </div>
-  )
+        <div className="w-full h-full flex items-center">
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full h-[40px] my-2 sm:h-4/6 px-5 "
+          >
+            {result ? result : "Submit now"}
+          </button>
+        </div>
+      </form>
+
+      <div className="row-start-6 col-span-2  w-full h-full">
+        <hr className="w-full bg-white" />
+        <br />
+        <p className="">&copy; 2025 Bikram Sharma</p>
+      </div>
+    </div>
+  );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,67 +1,119 @@
-import React, { useEffect } from 'react'
-import '../index.css'
+import React, { useEffect } from "react";
+import "../index.css";
 
 export default function Navbar() {
+  const navdisplayer = () => {
+    document.querySelector(".nav").classList.add("hidden");
+    document.querySelector(".smallnav").classList.remove("hidden").add("flex ");
+  };
 
-
-const navdisplayer = ()=>{
-  document.querySelector('.nav').classList.add("hidden")
-  document.querySelector('.smallnav').classList.remove("hidden").add('flex ')
-}
-
-const navhidder = ()=>{
-  document.querySelector('.smallnav').classList.add('hidden')
-  document.querySelector('.nav').classList.remove('hidden')
-}
-
-
+  const navhidder = () => {
+    document.querySelector(".smallnav").classList.add("hidden");
+    document.querySelector(".nav").classList.remove("hidden");
+  };
 
   return (
     <>
-    <div className='font-sans text-white flex justify-between items-center mx-[5%] sm:mx-[10%] h-[14vh] text-lg z-10'>
-      <div id='logo' className='text-5xl  flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>B</div>
-      <div className='w-1/2 h-4/6 align-middle flex items-center hidden sm:flex'>
-        <ul className='flex justify-between w-full'>
-          <a href="#" className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent'><li><p>Home</p></li></a>
-          <a href="#about" className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent'><li><p>About Me</p></li></a>
-          <a href="#project" className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent'><li><p>Projects</p></li></a>
-          <a href="#contact" className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent'><li><p>Contact</p></li></a>
-        </ul>
-      </div>
-      <div className='h-3/6 w-fit px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full flex items-center transition-transform duration-300 hover:scale-103 hidden sm:flex'>
-       <a href="#contact">
-        Connect With Me
-       </a>
-      </div>
+      <div className="font-sans text-white flex justify-between items-center mx-[5%] sm:mx-[10%] h-[14vh] text-lg z-10">
+        <div
+          id="logo"
+          className="text-5xl  flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent"
+        >
+          B
+        </div>
+        <div className="w-1/2 h-4/6 align-middle  items-center hidden sm:flex">
+          <ul className="flex justify-between w-full">
+            <a
+              href="#"
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent"
+            >
+              <li>
+                <p>Home</p>
+              </li>
+            </a>
+            <a
+              href="#about"
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent"
+            >
+              <li>
+                <p>About Me</p>
+              </li>
+            </a>
+            <a
+              href="#project"
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent"
+            >
+              <li>
+                <p>Projects</p>
+              </li>
+            </a>
+            <a
+              href="#contact"
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text hover:text-transparent"
+            >
+              <li>
+                <p>Contact</p>
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div className="h-3/6 w-fit px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full flex items-center transition-transform duration-300 hover:scale-103 hidden sm:flex">
+          <a href="#contact">Connect With Me</a>
+        </div>
 
-      <div className='flex sm:hidden nav' onClick={navdisplayer}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-9">
-         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-        </svg>
-      </div>
-
-     
-    </div>
-    <div className='smallnav hidden sm:hidden  h-screen w-50vw absolute top-1/2 left-1/2 right-0 -translate-y-1/2 bg-purple-800/80 z-10'>
-      <div className='h-25 flex justify-end items-center pr-[10%] text-white'>
-        <span onClick={navhidder}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-10">
-           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        <div className="flex sm:hidden nav" onClick={navdisplayer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="size-9"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+            />
           </svg>
-        </span>
-        
-     </div>
-     <div className='text-white p-[10%] h-2/6'>
-      <ul className='h-full flex flex-col justify-evenly'>
-        <a href="#"><li>Home</li></a>
-        <a href="#about"><li>About Me</li></a>
-        <a href="#project"><li>Projects</li></a>
-        <a href="#contact"><li>Contact</li></a>
-      </ul>
-     </div>
-    
-
-    </div>
+        </div>
+      </div>
+      <div className="smallnav hidden sm:hidden  h-screen w-50vw absolute top-1/2 left-1/2 right-0 -translate-y-1/2 bg-black z-10">
+        <div className="h-25 flex justify-end items-center pr-[10%] text-white">
+          <span onClick={navhidder}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </span>
+        </div>
+        <div className="text-white p-[10%] h-2/6">
+          <ul className="h-full flex flex-col justify-evenly">
+            <a href="#">
+              <li>Home</li>
+            </a>
+            <a href="#about">
+              <li>About Me</li>
+            </a>
+            <a href="#project">
+              <li>Projects</li>
+            </a>
+            <a href="#contact">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
+      </div>
     </>
-  )
+  );
 }
